b2434ca0010b1460392c7115f311ffdb27d3fd49
ZakirIndium <66434705+ZakirIndium@users.noreply.github.com>
2020-07-10 11:18:32 +0530

    new branch

--- pytest_configs/conftest.py
+++ pytest_configs/conftest.py
@@ -2,6 +2,7 @@ import pytest
 import os
 from Core.Configs import RunConfig
 
+
 @pytest.hookimpl(trylast=True)
 def pytest_sessionfinish(session, exitstatus):
     os.system('start ' + RunConfig.HTML_REPORT_FILE)
